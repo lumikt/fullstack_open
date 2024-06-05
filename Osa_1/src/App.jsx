abdaaea0f1e47cdb11ad0f9602b21a5fd45cba7b
Karri Lumivirta <lumivirta.karri@hotmail.com>
2024-06-05 15:44:03 +0300

    Tehtävä 1.3

--- Osa_1/src/App.jsx
+++ Osa_1/src/App.jsx
@@ -1,43 +1,61 @@
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   return (
     <div>
-      <Header course='Half Stack application development'/>
-      <Content />
-      <Total exercises1={10} exercises2={7} exercises3={14}/>
+      <Header name={course}/>
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
     </div>
   )
 }
 
 
 const Header = (props) => {
+  console.log(props)
   return (
   <>
-    <h1>{props.course}</h1>
+    <h1>{props.name}</h1>
   </>
   )
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
   <>
-    <Part name='Fundamentals of React' excercise={10}/>
-    <Part name='Using props to pass data' excercise={7}/>
-    <Part name='State of a component' excercise={14}/>
+    <Part name={props.part1.name} exercise={props.part1.exercises}/>
+    <Part name={props.part2.name} exercise={props.part2.exercises}/>
+    <Part name={props.part3.name} exercise={props.part3.exercises}/>
   </>
   )
 }
 
 const Part = (props) => {
+  console.log(props)
   return (
     <>
     <p>
-      {props.name} {props.excercise}
+      {props.name} {props.exercise}
     </p>
     </>
   )
 }
 
 const Total = (props) => {
+  console.log(props)
   return (
   <>
     <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
