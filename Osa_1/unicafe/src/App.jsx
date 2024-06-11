f851b0ddf1cc9441f7c6b1b20491e501e420a9fb
Karri Lumivirta <lumivirta.karri@hotmail.com>
2024-06-11 13:36:36 +0300

    Tehtävä 1.7

--- Osa_1/unicafe/src/App.jsx
+++ Osa_1/unicafe/src/App.jsx
@@ -13,21 +13,33 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleGoodClick = () => {
     console.log('Increasing good count, prev value', good)
-    setGood(good +1)
+    const newGood = good +1
+    setGood(newGood)
+    setTotal(newGood + neutral + bad)
   }
 
   const handleNeutralClick = () => {
     console.log('Increasing neutral count, prev value', neutral)
-    setNeutral(neutral +1)
+    const newNeutral = neutral + 1
+    setNeutral(newNeutral)
+    setTotal(good + newNeutral + bad)
+
   }
 
   const handleBadClick = () => {
     console.log('Increasing bad count, prev value', bad)
-    setBad(bad +1)
+    const newBad = bad +1
+    setBad(newBad)
+    setTotal(good + neutral + newBad)
   }
+
+  const statAverage = () => (
+    (good+bad+neutral)/total
+  )
 
   return (
     <div>
@@ -39,7 +51,10 @@ const App = () => {
     <p>
       good {good}<br/>
       neutral {neutral}<br/>
-      bad {bad}
+      bad {bad}<br/>
+      total {total}<br/>
+      average {(good+bad*-1)/total}<br/>
+      positive {(good/total)*100} %
     </p>
     </div>
   )
