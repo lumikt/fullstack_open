1a4d1d1a6c677bb050976d2ea50c51ed496063af
Karri Lumivirta <lumivirta.karri@hotmail.com>
2026-08-28 12:40:50 +0300

    Update weather api & commit package locks

--- osa_2/maiden_tiedot/src/services/weather.js
+++ osa_2/maiden_tiedot/src/services/weather.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
-const api_key = import.meta.env.VITE_SOME_KEY
-
+const api_key = import.meta.env.VITE_WEATHER_KEY
+console.log(api_key)
 const getAll = ( city) => {
     return axios.get(`${baseUrl}${city}&appid=${api_key}&units=metric`)
 }
